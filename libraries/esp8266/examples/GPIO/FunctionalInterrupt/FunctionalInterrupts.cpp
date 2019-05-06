5a144258c588cfc2bf1629815cdb3258927a6fce
Dirk O. Kaar <dok@dok-net.net>
2019-05-06 02:38:44 +0200

    astyle libraries/esp8266/examples/GPIO/FunctionalInterrupt

--- libraries/esp8266/examples/GPIO/FunctionalInterrupt/FunctionalInterrupts.cpp
+++ libraries/esp8266/examples/GPIO/FunctionalInterrupt/FunctionalInterrupts.cpp
@@ -3,26 +3,32 @@
 #include <Arduino.h>
 #include <memory>
 
-void ICACHE_RAM_ATTR interruptFunctional(const ArgStructure& localArg) {
-  if (localArg.reqScheduledFunction) {
-    if (localArg.interruptInfo) {
-      localArg.interruptInfo->value = digitalRead(localArg.interruptInfo->pin);
-      localArg.interruptInfo->micro = micros();
+void ICACHE_RAM_ATTR interruptFunctional(const ArgStructure& localArg)
+{
+    if (localArg.reqScheduledFunction)
+    {
+        if (localArg.interruptInfo)
+        {
+            localArg.interruptInfo->value = digitalRead(localArg.interruptInfo->pin);
+            localArg.interruptInfo->micro = micros();
+        }
+        schedule_function(
+            [reqScheduledFunction = localArg.reqScheduledFunction,
+                                  interruptInfo = *localArg.interruptInfo]()
+        {
+            reqScheduledFunction(interruptInfo);
+        });
     }
-    schedule_function(
-      [reqScheduledFunction = localArg.reqScheduledFunction,
-                         interruptInfo = *localArg.interruptInfo]() {
-      reqScheduledFunction(interruptInfo);
-    });
-  }
 }
 
-void attachScheduledInterrupt(uint8_t pin, std::function<void(const InterruptInfo&)> scheduledIntRoutine, int mode) {
-  std::shared_ptr<ArgStructure> arg(new ArgStructure());
-  arg->interruptInfo = new InterruptInfo(pin);
-  arg->reqScheduledFunction = scheduledIntRoutine;
+void attachScheduledInterrupt(uint8_t pin, std::function<void(const InterruptInfo&)> scheduledIntRoutine, int mode)
+{
+    std::shared_ptr<ArgStructure> arg(new ArgStructure());
+    arg->interruptInfo = new InterruptInfo(pin);
+    arg->reqScheduledFunction = scheduledIntRoutine;
 
-  attachInterrupt(pin, [arg = move(arg)]() {
-    interruptFunctional(*arg);
-  }, mode);
+    attachInterrupt(pin, [arg = move(arg)]()
+    {
+        interruptFunctional(*arg);
+    }, mode);
 }
--- libraries/esp8266/examples/GPIO/FunctionalInterrupt/FunctionalInterrupts.h
+++ libraries/esp8266/examples/GPIO/FunctionalInterrupt/FunctionalInterrupts.h
@@ -6,40 +6,46 @@
 
 // Structures for communication
 
-struct InterruptInfo {
-  InterruptInfo(uint8_t _pin) : pin(_pin) {}
-  const uint8_t pin;
-  uint8_t value = 0;
-  uint32_t micro = 0;
+struct InterruptInfo
+{
+    InterruptInfo(uint8_t _pin) : pin(_pin) {}
+    const uint8_t pin;
+    uint8_t value = 0;
+    uint32_t micro = 0;
 };
 
-struct ArgStructure {
-  ArgStructure() = default;
-  ~ArgStructure() {
-    delete interruptInfo;
-  }
-  ArgStructure(const ArgStructure& as) noexcept :
-    interruptInfo(as.interruptInfo),
-    reqScheduledFunction(as.reqScheduledFunction)
-  { }
-  ArgStructure(ArgStructure&& as) noexcept :
-    interruptInfo(as.interruptInfo),
-    reqScheduledFunction(as.reqScheduledFunction) {
-    as.interruptInfo = nullptr;
-  }
-  ArgStructure& operator=(const ArgStructure& as) noexcept {
-    interruptInfo = as.interruptInfo;
-    reqScheduledFunction = as.reqScheduledFunction;
-    return *this;
-  }
-  ArgStructure& operator=(ArgStructure&& as) noexcept {
-    interruptInfo = as.interruptInfo;
-    as.interruptInfo = nullptr;
-    reqScheduledFunction = as.reqScheduledFunction;
-    return *this;
-  }
-  InterruptInfo* interruptInfo = nullptr;
-  std::function<void(const InterruptInfo&)> reqScheduledFunction = nullptr;
+struct ArgStructure
+{
+    ArgStructure() = default;
+    ~ArgStructure()
+    {
+        delete interruptInfo;
+    }
+    ArgStructure(const ArgStructure& as) noexcept :
+        interruptInfo(as.interruptInfo),
+        reqScheduledFunction(as.reqScheduledFunction)
+    { }
+    ArgStructure(ArgStructure&& as) noexcept :
+        interruptInfo(as.interruptInfo),
+        reqScheduledFunction(as.reqScheduledFunction)
+    {
+        as.interruptInfo = nullptr;
+    }
+    ArgStructure& operator=(const ArgStructure& as) noexcept
+    {
+        interruptInfo = as.interruptInfo;
+        reqScheduledFunction = as.reqScheduledFunction;
+        return *this;
+    }
+    ArgStructure& operator=(ArgStructure&& as) noexcept
+    {
+        interruptInfo = as.interruptInfo;
+        as.interruptInfo = nullptr;
+        reqScheduledFunction = as.reqScheduledFunction;
+        return *this;
+    }
+    InterruptInfo* interruptInfo = nullptr;
+    std::function<void(const InterruptInfo&)> reqScheduledFunction = nullptr;
 };
 
 void attachScheduledInterrupt(uint8_t pin, std::function<void(const InterruptInfo&)> scheduledIntRoutine, int mode);
