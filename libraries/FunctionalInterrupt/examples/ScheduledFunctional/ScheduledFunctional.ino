1b42e1d9182caf356a3980556ba39aea242a0664
Dirk O. Kaar <dok@dok-net.net>
2020-01-22 11:39:04 +0100

    Apply astyle for ESP8266 examples.

--- libraries/FunctionalInterrupt/examples/ScheduledFunctional/ScheduledFunctional.ino
+++ libraries/FunctionalInterrupt/examples/ScheduledFunctional/ScheduledFunctional.ino
@@ -16,29 +16,29 @@
 #endif
 
 class Button {
-public:
+  public:
     Button(uint8_t reqPin) : PIN(reqPin) {
-        pinMode(PIN, INPUT_PULLUP);
-        attachScheduledInterrupt(PIN, [this](const InterruptInfo& ii) {
-            Serial.print("Pin ");
-            Serial.println(ii.pin);
-            numberKeyPresses += 1;
-            pressed = true;
-            }, FALLING); // works on ESP8266
+      pinMode(PIN, INPUT_PULLUP);
+      attachScheduledInterrupt(PIN, [this](const InterruptInfo & ii) {
+        Serial.print("Pin ");
+        Serial.println(ii.pin);
+        numberKeyPresses += 1;
+        pressed = true;
+      }, FALLING); // works on ESP8266
     };
     ~Button() {
-        detachInterrupt(PIN);
+      detachInterrupt(PIN);
     }
 
     uint32_t checkPressed() {
-        if (pressed) {
-            Serial.printf("Button on pin %u has been pressed %u times\n", PIN, numberKeyPresses);
-            pressed = false;
-        }
-        return numberKeyPresses;
+      if (pressed) {
+        Serial.printf("Button on pin %u has been pressed %u times\n", PIN, numberKeyPresses);
+        pressed = false;
+      }
+      return numberKeyPresses;
     }
 
-private:
+  private:
     const uint8_t PIN;
     volatile uint32_t numberKeyPresses = 0;
     volatile bool pressed = false;
@@ -49,21 +49,21 @@ Button* button2;
 
 
 void setup() {
-    Serial.begin(115200);
-    while (!Serial) {}
-    delay(500);
-    Serial.println("FunctionalInterrupt test/example");
+  Serial.begin(115200);
+  while (!Serial) {}
+  delay(500);
+  Serial.println("FunctionalInterrupt test/example");
 
-    button1 = new Button(BUTTON1);
-    button2 = new Button(BUTTON2);
+  button1 = new Button(BUTTON1);
+  button2 = new Button(BUTTON2);
 
-    Serial.println("setup() complete");
+  Serial.println("setup() complete");
 }
 
 void loop() {
-    button1->checkPressed();
-    if (nullptr != button2 && 10 < button2->checkPressed()) {
-        delete button2;
-        button2 = nullptr;
-    }
+  button1->checkPressed();
+  if (nullptr != button2 && 10 < button2->checkPressed()) {
+    delete button2;
+    button2 = nullptr;
+  }
 }
