2c671f53ed4fef81c159259c29d13b5a924e2a6b
Dirk O. Kaar <dok@dok-net.net>
2021-01-01 18:20:31 +0100

    all-in functional squash commit.

--- cores/esp8266/Arduino.h
+++ cores/esp8266/Arduino.h
@@ -225,6 +225,7 @@ void optimistic_yield(uint32_t interval_us);
 #include <algorithm>
 #include <cstdlib>
 #include <cmath>
+#include "Delegate.h"
 
 
 #include "mmu_iram.h"
@@ -268,6 +269,8 @@ void setTZ(const char* tz);
 
 void configTime(int timezone, int daylightOffset_sec, const char* server1,
     const char* server2 = nullptr, const char* server3 = nullptr);
+
+void attachInterrupt(uint8_t pin, Delegate<void(), void*> userFunc, int mode);
 
 void configTime(const char* tz, const char* server1,
     const char* server2 = nullptr, const char* server3 = nullptr);
